18cd6e6e1d901b342c04bd33ed6489c8d2be5821
Kristian Surya Dinata <94119881+Kristian2828@users.noreply.github.com>
2022-01-08 18:54:49 +0800

    Update app.R

--- app.R
+++ app.R
@@ -35,7 +35,7 @@ ui <- fluidPage(
     
     tabPanel("Introduction", value = "Introduction", tags$img(src = "coronavirus.jpg", height = "300", width = "1875", align="centre"),
       sidebarLayout(
-        sidebarPanel(h5(strong("Coronavirus disease (COVID-19)")," is an infectious disease caused by the SARS-CoV-2 virus.
+        sidebarPanel(h3(strong("Coronavirus disease (COVID-19)")," is an infectious disease caused by the SARS-CoV-2 virus.
 
 Most people infected with the virus will experience mild to moderate respiratory illness and recover without requiring special treatment. However, some will become seriously ill and require medical attention. Older people and those with underlying medical conditions like cardiovascular disease, diabetes, chronic respiratory disease, or cancer are more likely to develop serious illness. Anyone can get sick with COVID-19 and become seriously ill or die at any age. 
 
