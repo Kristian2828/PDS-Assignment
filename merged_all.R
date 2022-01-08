5800bcd527a5015799009ce2d912af20d74c9d78
Kristian Surya Dinata <94119881+Kristian2828@users.noreply.github.com>
2022-01-08 17:56:42 +0800

    Update merged_all.R

--- merged_all.R
+++ merged_all.R
@@ -35,13 +35,13 @@ ui <- fluidPage(
     
     tabPanel("Introduction", value = "Introduction", tags$img(src = "coronavirus.jpg", height = "300", width = "1875", align="centre"),
       sidebarLayout(
-        sidebarPanel(strong("Coronavirus disease (COVID-19)")," is an infectious disease caused by the SARS-CoV-2 virus.
+        sidebarPanel(h4(strong("Coronavirus disease (COVID-19)")," is an infectious disease caused by the SARS-CoV-2 virus.
 
 Most people infected with the virus will experience mild to moderate respiratory illness and recover without requiring special treatment. However, some will become seriously ill and require medical attention. Older people and those with underlying medical conditions like cardiovascular disease, diabetes, chronic respiratory disease, or cancer are more likely to develop serious illness. Anyone can get sick with COVID-19 and become seriously ill or die at any age. 
 
-The best way to prevent and slow down transmission is to be well informed about the disease and how the virus spreads. Protect yourself and others from infection by staying at least 1 metre apart from others, wearing a properly fitted mask, and washing your hands or using an alcohol-based rub frequently. Get vaccinated when it's your turn and follow local guidance.
+The best way to prevent and slow down transmission is to be well informed about the disease and how the virus spreads. Protect yourself and others from infection by staying at least 1 metre apart from others, wearing a properly fitted mask, and washing your hands or using an alcohol-based rub frequently. Get vaccinated when it’s your turn and follow local guidance.
 
-The virus can spread from an infected person's mouth or nose in small liquid particles when they cough, sneeze, speak, sing or breathe. These particles range from larger respiratory droplets to smaller aerosols. It is important to practice respiratory etiquette, for example by coughing into a flexed elbow, and to stay home and self-isolate until you recover if you feel unwell. (WHO, 2021)", align="centre"),
+The virus can spread from an infected person’s mouth or nose in small liquid particles when they cough, sneeze, speak, sing or breathe. These particles range from larger respiratory droplets to smaller aerosols. It is important to practice respiratory etiquette, for example by coughing into a flexed elbow, and to stay home and self-isolate until you recover if you feel unwell"), br(), em("Data is retrieved from www.who.com"), align="justified"),
         
         mainPanel(
           tabsetPanel(
@@ -93,7 +93,7 @@ chest pain.",br(),
           dateRangeInput(inputId = "date", "Date range",
                          start = min(covid$Date),
                          end   = max(covid$Date)),
-          downloadButton(outputId = "download_data", label = "Download"),
+          downloadButton(outputId = "download_data", label = "Download"),br(),br(), em("Data is retrieved from www.worldometers.info"),
     ),
       mainPanel(
         plotlyOutput(outputId = "plot"), br(),
@@ -225,7 +225,7 @@ tabPanel("Vaccinnation", value = "Vaccination",
 
   tabPanel("About us", value = "About us",
            sidebarLayout(
-             sidebarPanel(strong(h4("PREPARED BY"), br(),strong("Kristian Alexander , Daniel , Eva , Shu Mei and Ezlan"))),
+             sidebarPanel(strong(h4("PREPARED BY"), br(),strong("1. Kristian Surya Dinata | S2043845", br(), "2. Muhammad Ezlan Bin Zakhir | S2116731", br(), "3. Muhammad Amirul Daniel bin Badrul Hisham | S2115750", br(), "4. Eva Chin Ying Ming | S2115794", br(), "5. Law Shu Meei | S2117388"))),
               mainPanel(""),
 ))))
 
